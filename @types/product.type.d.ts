f9a25e8b6953ca98c0b8a0bcf82029f3290600b7
Squirrel68 <constantlypeaceinside@gmail.com>
2025-05-05 22:19:00 +0700

    fix: update product handling to support priceBeforeDiscount field

--- @types/product.type.d.ts
+++ @types/product.type.d.ts
@@ -1,13 +1,14 @@
 interface Product {
-  name: string,
-  image: string,
-  images: string[],
-  description: string,
-  category: string[],
-  price: number,
-  price_before_discount: number,
-  quantity: number,
-  rating: number,
-  view: number,
-  sold: number,
+  name: string
+  image: string
+  images: string[]
+  description: string
+  category: string[]
+  price: number
+  price_before_discount: number
+  priceBeforeDiscount?: number
+  quantity: number
+  rating: number
+  view: number
+  sold: number
 }
